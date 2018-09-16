008bc3347e454968ad2dadfd738e228c9274fa83
Alexander <zam.kuratora@gmail.com>
2018-09-16 14:55:21 +0300

    Update RaeDatabase.cs

--- RAExt/RaeDatabase.cs
+++ RAExt/RaeDatabase.cs
@@ -1,99 +1,112 @@
-﻿using AutoMapper;
 using Realms;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace RAExt
 {
-    public class RaeDatabase
+    public class RaeDatabase : IDisposable
     {
         public delegate void ProgressDelegate(int current, int total);
         public delegate void ErrorDelegate(string message);
         public delegate void CompleteDelegate();
-        
+
         public event ProgressDelegate OnProgress;
         public event ErrorDelegate OnError;
         public event CompleteDelegate OnComplete;
 
         public Realm realm;
-        
+
+        /// <summary>
+        /// Items cached.
+        /// </summary>
+        ConcurrentDictionary<int, ItemData> cache = new ConcurrentDictionary<int, ItemData>();
+
         public RaeDatabase(string filename = "rae_items.realm")
         {
             InitRealm(filename);
         }
 
+        /// <summary>
+        /// Load realm db.
+        /// </summary>
+        /// <param name="filename">Db file name.</param>
         public void InitRealm(string filename = "rae_items.realm")
         {
             var config = new RealmConfiguration(filename);
             RealmConfiguration.DefaultConfiguration = config;
-            realm = Realm.GetInstance();   
+            realm = Realm.GetInstance();
         }
 
-        public void SaveToRealm(ItemDataUnmanaged[] items)
-        {
-            var managedItems = new ItemData[items.Length];
-            for (int i = 0; i < items.Length; i++)
-            {
-                managedItems[i] = ItemData.FromUnmanaged(items[i]);
-            }
-            SaveToRealm(managedItems);
-        }
+        /// <summary>
+        /// Save items to realm.
+        /// </summary>
+        /// <param name="items">Unma</param>
+        public void SaveToRealm(IEnumerable<ItemDataUnmanaged> items) =>
+            SaveToRealm(items.Select(x => ItemData.FromUnmanaged(x)));
 
-        public void SaveToRealm(ItemData[] items)
+        /// <summary>
+        /// Saving items to realm.
+        /// </summary>
+        /// <param name="items">Items.</param>
+        public void SaveToRealm(IEnumerable<ItemData> items)
         {
-            int current = 0, total = items.Length;
+            int current = 0, total = items.Count();
             using (var trans = realm.BeginWrite())
             {
-	            //realm.RemoveAll()
-				foreach (var itemData in items)
+                foreach (var itemData in items)
                 {
                     realm.Add(itemData);
 
-                    if (OnProgress == null) continue;
                     if (++current % 10 == 0 || current == total)
-                        OnProgress(current, total);
+                        OnProgress?.Invoke(current, total);
                 }
                 trans.Commit();
             }
-            
-            Realm.Compact();
-            realm.Dispose();
-	        OnComplete?.Invoke();
+
+            Dispose();
+            OnComplete?.Invoke();
         }
 
-	    public void RemoveAll() => realm.Write(() => realm.RemoveAll());
-	    public void CloseRealm()
-	    {
-		    Realm.Compact();
-		    realm.Dispose();
-	    }
-	    
-	    
-			//Realm.DeleteRealm(RealmConfiguration.DefaultConfiguration);
+        /// <summary>
+        /// Remove all items.
+        /// </summary>
+        public void RemoveAll() => realm.Write(() => realm.RemoveAll());
 
-	 //   private Transaction transaction;
-		//public void BeginWrite()
-		//{
-		//	transaction = realm.BeginWrite();
-		//}
-		//public void AddItem()
-		//{
-		//	transaction = realm.BeginWrite();
-		//}
-		//public void EndWrite()
-		//{
-		//	transaction.Commit();
-		//	transaction.Dispose();
-		//}
-
-
-		public ItemData GetItem(int id, ItemType itemType = ItemType.Matter)
+        /// <summary>
+        /// Getting item data by item id.
+        /// </summary>
+        /// <param name="id">Item ID.</param>
+        /// <param name="itemType">Item type.</param>
+        public ItemData GetItem(int id, ItemType itemType = ItemType.Matter)
         {
-			if (realm == null) InitRealm();
-	        if(itemType > ItemType._PW_STARTS_HERE_)
-		        return realm.All<ItemData>().FirstOrDefault(i => i.Id == id && i.iListType == (int)itemType);
-		    else
-				return realm.All<ItemData>().FirstOrDefault(i => i.Id == id && i.iItemType == (int)itemType);
-		}
+            if (realm == null)
+                InitRealm();
+
+            if (cache.ContainsKey(id))
+                return cache[id];
+
+            ItemData item = null;
+
+            if (itemType > ItemType._PW_STARTS_HERE_)
+                item = realm.All<ItemData>().FirstOrDefault(i => i.Id == id && i.iListType == (int)itemType);
+            else item = realm.All<ItemData>().FirstOrDefault(i => i.Id == id && i.iItemType == (int)itemType);
+
+            if (item != null)
+                cache.TryAdd(id, item);
+
+            return item;
+        }
+
+        /// <summary>
+        /// Release resources.
+        /// </summary>
+        public void Dispose()
+        {
+            cache.Clear();
+            Realm.Compact();
+            realm.Dispose();
+        }
     }
 }
