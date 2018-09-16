cc7e89d5ec3ddaa492e1dcd642efb8111f4b0bed
Alexander <zam.kuratora@gmail.com>
2018-09-16 14:57:49 +0300

    Update ItemDataUnmanaged.cs

--- RAExt/Structs/ItemDataUnmanaged.cs
+++ RAExt/Structs/ItemDataUnmanaged.cs
@@ -1,30 +1,89 @@
-﻿
 using AutoMapper;
 using Newtonsoft.Json;
 
 namespace RAExt
 {
     using static Globals;
-    
+
+    /// <summary>
+    /// Unmanaged item data.
+    /// </summary>
     public class ItemDataUnmanaged
     {
+        /// <summary>
+        /// Item type.
+        /// </summary>
         public int iItemType { get; set; } //ItemType
+
+        /// <summary>
+        /// List type.
+        /// </summary>
         public int iListType { get; set; } //ItemType
+
+        /// <summary>
+        /// Item id in elements.data
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Item name in elements.data
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Item color in elements.data
+        /// </summary>
         public int ColorArgb { get; set; }
+
+        /// <summary>
+        /// Item icon.
+        /// </summary>
         public byte[] IconBytes { get; set; }
+
+        /// <summary>
+        /// Item description in configs.pck
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Amount in stack in elements.data
+        /// </summary>
         public int StackAmount { get; set; }
+
+        /// <summary>
+        /// Item mask in elements.data
+        /// </summary>
         public int Mask { get; set; }
+
+        /// <summary>
+        /// Item proctype (bit masks) in elements.data
+        /// </summary>
         public int ProcType { get; set; }
+
+        /// <summary>
+        /// Item octet from RAE.
+        /// </summary>
         public byte[] Octet { get; set; }
+
+        /// <summary>
+        /// Html content.
+        /// </summary>
         public string HtmlContent { get; set; }
-        
-        public ItemData ToManaged() 
+
+        /// <summary>
+        /// Convert to managed item data.
+        /// </summary>
+        /// <returns></returns>
+        public ItemData ToManaged()
             => RMapper.Map<ItemData>(this);
 
         private bool saveIconBytes;
+
+        /// <summary>
+        /// Getting json from unmanaged item data.
+        /// </summary>
+        /// <param name="saveIconBytes">Save icon bytes.</param>
+        /// <returns></returns>
         public string ToJson(bool saveIconBytes = true)
         {
             this.saveIconBytes = saveIconBytes;
@@ -33,10 +92,20 @@ namespace RAExt
         //For Json lib:
         public bool ShouldSerializeIconBytes() => saveIconBytes;
 
-        public static ItemDataUnmanaged FromManaged(ItemData managed) 
-            => RMapper.Map<ItemDataUnmanaged>(managed);
-        public static ItemDataUnmanaged FromJson(string json)
-            => JsonConvert.DeserializeObject<ItemDataUnmanaged>(json);
+        /// <summary>
+        /// Convert managed item data to unmanaged.
+        /// </summary>
+        /// <param name="managed">Managed item data.</param>
+        /// <returns></returns>
+        public static ItemDataUnmanaged FromManaged(ItemData managed) =>
+            RMapper.Map<ItemDataUnmanaged>(managed);
 
+        /// <summary>
+        /// Parse json to item data.
+        /// </summary>
+        /// <param name="json">Json.</param>
+        /// <returns></returns>
+        public static ItemDataUnmanaged FromJson(string json) =>
+            JsonConvert.DeserializeObject<ItemDataUnmanaged>(json);
     }
 }
